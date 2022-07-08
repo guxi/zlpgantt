76b9c44e97324024bdab74e846c2ae26f9fcaed4
GUXI\guxi <guxi@live.com>
2022-07-08 17:50:31 +0800

    7-8

--- src/datatable.js
+++ src/datatable.js
@@ -1,17 +1,18 @@
 
+
 const setdataTable = function (data, divid) {
-
-  var div = document.getElementById("data_Table_div");
-  let data_Table = document.getElementById("data_Table");
-
-  let thead = document.getElementById("data_thead");
-  let tbody = document.getElementById("data_tbody");
-
+  let dragdiv = document.getElementById("drag_test");
+  let div = document.getElementById("data_Table_div");
   let title = JSON.parse(data).title;
   let tr_data = JSON.parse(data).data;
-
+  let table_R = document.createElement("table");
   let thead_R = document.createElement("thead");
   let tbody_R = document.createElement("tbody");
+  let Div_R = document.createElement("div");
+
+  Div_R.setAttribute("id", "data_Table_div");
+  table_R.setAttribute("class", "table table-striped");
+
   let trh = document.createElement("tr");
   title.forEach(e => {
     let th = document.createElement("th");
@@ -19,8 +20,6 @@ const setdataTable = function (data, divid) {
     trh.appendChild(th);
   })
   thead_R.appendChild(trh);
-  data_Table.replaceChild(thead_R, thead);
-
 
   tr_data.forEach(e => {
     let trd = document.createElement("tr");
@@ -34,9 +33,71 @@ const setdataTable = function (data, divid) {
     })
     tbody_R.appendChild(trd)
   })
-  data_Table.replaceChild(tbody_R, tbody);
-
+  table_R.appendChild(thead_R);
+  table_R.appendChild(tbody_R);
+  Div_R.append(table_R);
+  dragdiv.replaceChild(Div_R, div);
 
 }
 
+
 export { setdataTable };
+
+
+
+// const setdataTable = function (data, divid) {
+
+//   var div = document.getElementById("data_Table_div");
+//   let data_Table = document.getElementById("data_Table");
+
+
+
+
+
+
+
+
+//   let thead = document.getElementById("data_thead");
+//   let tbody = document.getElementById("data_tbody");
+
+//   let title = JSON.parse(data).title;
+//   let tr_data = JSON.parse(data).data;
+
+//   let table_R = document.createElement("table");
+//   let thead_R = document.createElement("thead");
+//   let tbody_R = document.createElement("tbody");
+//   let trh = document.createElement("tr");
+
+//   table_R.setAttribute("class", "table table-striped");
+//   table_R.setAttribute("id", "data_Table");
+
+//   title.forEach(e => {
+//     let th = document.createElement("th");
+//     th.innerHTML = e;
+//     trh.appendChild(th);
+//   })
+//   thead_R.appendChild(trh);
+//   data_Table.replaceChild(thead_R, thead);
+
+
+//   tr_data.forEach(e => {
+//     let trd = document.createElement("tr");
+//     e.forEach(ee => {
+//       let td
+//       if (typeof ee == "string") { td = document.createElement("th"); }
+//       else td = document.createElement("td");
+
+//       td.innerHTML = ee;
+//       trd.appendChild(td);
+//     })
+//     tbody_R.appendChild(trd)
+//   })
+
+//   //table_R.replaceChild(tbody_R, tbody);
+
+//   //data_Table_div.replaceChild(data_Table, table_R);
+
+
+// }
+
+// export { setdataTable };
